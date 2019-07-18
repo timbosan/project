a05607b24bfe8e32ca8d4e5e5fbcf5b556ac2f05
Timothy Chea <tac.chea@gmail.com>
2019-07-18 19:23:34 -0400

    project update now with js

---
+++ restaurantasiajavascript.js
@@ -0,0 +1,12 @@
+
+/* option 1
+alert("Do you like Filipino Food?");
+*/
+
+var txt;
+var r = confirm("Do You Like Filipino Food?");
+if (r == true) {
+  alert ("Come Right In!");
+} else {
+  alert("Let's Change your mind");
+}
